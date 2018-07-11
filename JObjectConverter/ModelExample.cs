5984a9f475be53931cf80c526631eb21669bb725
cristiano motta <cristiano.motta@hotmail.it>
2018-07-11 22:16:51 +0200

    First commit

---
+++ JObjectConverter/ModelExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace JObjectCoverter
+{
+    public class ModelExample
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public DateTime Today { get; set; }
+        public JObject Data { get; set; }
+    }
+}
--- JObjectConverter/Program.cs
+++ JObjectConverter/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
-namespace JObjectConverter
+namespace JObjectCoverter
 {
     class Program
     {
+        /// <summary>
+        /// Set snake name convention for deserialization/serialization
+        /// </summary>
+        private static readonly SnakeCaseNamingStrategy SnakeNameStrategy = new SnakeCaseNamingStrategy();
+        private static readonly JsonSerializerSettings JsonSerializerSettings =
+            new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                Converters = new[]
+                    {new JObjectCustomConverter(SnakeNameStrategy)},
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = SnakeNameStrategy
+                }
+            };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DeserializeJObjectTest();
+            Console.ReadKey();
+        }
+
+        private static void DeserializeJObjectTest()
+        {
+            var text = @"{
+                'nome': 'ciao',
+                'id': '14',
+                'today': '12/05/2017',
+                'data': {
+                    'first_name': 'Cristiano',
+                    'last_name': 'Motta',
+                   }
+               }";
+
+            // Deserialize json into typed model
+            var model = DeserializeJson<ModelExample>(text);
+            Console.WriteLine(model.Data);
+        }
+
+        private static void SerializeJObjectTest()
+        {
+            var text = @"{
+                'Nominativo': 'ciao',
+                'IndirizzoDiFatturazione': 'via tal dei tali',
+                'FatturaDaInviare': '122222',
+                'NestedObject': {
+                    'FirstName': 'Cristiano',
+                    'LastName': 'Motta',
+                   }
+               }";
+
+            JObject jobject = JObject.Parse(text);
+            var json = SerializeJson(jobject);
+            Console.WriteLine(json);
+        }
+
+        private static T DeserializeJson<T>(string request)
+        {
+            var obj = JsonConvert.DeserializeObject<T>(request, JsonSerializerSettings);
+            return obj;
+        }
+
+        private static string SerializeJson(object request)
+        {
+            var json = JsonConvert.SerializeObject(request, JsonSerializerSettings);
+            return json;
         }
     }
 }
